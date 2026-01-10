8ba16b2aeba403f2197eb878d6b2020a0661d6dd
minseeeeo <alstj09162@gmail.com>
2026-01-10 21:28:19 +0900

    feat: 헤더 컴포넌트 개발

--- ToSea-FE/src/layout/Layout.tsx
+++ ToSea-FE/src/layout/Layout.tsx
@@ -3,11 +3,11 @@ import Header from '../components/common/Header'
 
 export const Layout = () => {
 	return (
-		<>
+		<div className='relative bg-blue-500'>
 			<Header />
 			<div className='w-full max-w-main mx-auto'>
 				<Outlet />
 			</div>
-		</>
+		</div>
 	)
 }
--- ToSea-FE/src/pages/MainPage.tsx
+++ ToSea-FE/src/pages/MainPage.tsx
@@ -1,8 +1,50 @@
 const MainPage = () => {
 	return (
-		<>
-			<div className='title-2 bg-gray-8 text-blue-100 py-20 rounded-12'>메인 페이지~!</div>
-		</>
+		<div className='min-h-screen text-white pt-14'>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id aspernatur vel debitis earum suscipit perspiciatis
+			fugit officia ut reiciendis officiis tempore maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum
+			dolor sit amet consectetur adipisicing elit. Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio
+			facilis obcaecati rerum quidem aliquam quis repellat optio amet ratione nesciunt fuga. Lorem, ipsum dolor sit amet
+			consectetur adipisicing elit. Ipsam, id aspernatur vel debitis earum suscipit perspiciatis fugit officia ut reiciendis
+			officiis tempore maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur
+			adipisicing elit. Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio facilis obcaecati rerum
+			quidem aliquam quis repellat optio amet ratione nesciunt fuga. Lorem, ipsum dolor sit amet consectetur adipisicing
+			elit. Ipsam, id aspernatur vel debitis earum suscipit perspiciatis fugit officia ut reiciendis officiis tempore
+			maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio facilis obcaecati rerum quidem aliquam quis
+			repellat optio amet ratione nesciunt fuga.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id
+			aspernatur vel debitis earum suscipit perspiciatis fugit officia ut reiciendis officiis tempore maiores nihil. Eum
+			explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi alias hic quod
+			quisquam. Libero delectus, quas minima distinctio facilis obcaecati rerum quidem aliquam quis repellat optio amet
+			ratione nesciunt fuga.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id aspernatur vel debitis earum
+			suscipit perspiciatis fugit officia ut reiciendis officiis tempore maiores nihil. Eum explicabo dolores quod libero
+			delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi alias hic quod quisquam. Libero delectus, quas
+			minima distinctio facilis obcaecati rerum quidem aliquam quis repellat optio amet ratione nesciunt fuga.Lorem, ipsum
+			dolor sit amet consectetur adipisicing elit. Ipsam, id aspernatur vel debitis earum suscipit perspiciatis fugit
+			officia ut reiciendis officiis tempore maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum dolor sit
+			amet consectetur adipisicing elit. Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio facilis
+			obcaecati rerum quidem aliquam quis repellat optio amet ratione nesciunt fuga.uscipit perspiciatis fugit officia ut
+			reiciendis officiis tempore maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet
+			consectetur adipisicing elit. Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio facilis obcaecati
+			rerum quidem aliquam quis repellat optio amet ratione nesciunt fuga.Lorem, ipsum dolor sit amet consectetur
+			adipisicing elit. Ipsam, id aspernatur vel debitis earum suscipit perspiciatis fugit officia ut reiciendis officiis
+			tempore maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur adipisicing
+			elit. Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio facilis obcaecati rerum quidem aliquam
+			quis repellat optio amet ratione nesciunt fuga.uscipit perspiciatis fugit officia ut reiciendis officiis tempore
+			maiores nihil. Eum explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Quasi alias hic quod quisquam. Libero delectus, quas minima distinctio facilis obcaecati rerum quidem aliquam quis
+			repellat optio amet ratione nesciunt fuga.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id
+			aspernatur vel debitis earum suscipit perspiciatis fugit officia ut reiciendis officiis tempore maiores nihil. Eum
+			explicabo dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi alias hic quod
+			quisquam. Libero delectus, quas minima distinctio facilis obcaecati rerum quidem aliquam quis repellat optio amet
+			ratione nesciunt fuga.uscipit perspiciatis fugit officia ut reiciendis officiis tempore maiores nihil. Eum explicabo
+			dolores quod libero delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi alias hic quod quisquam.
+			Libero delectus, quas minima distinctio facilis obcaecati rerum quidem aliquam quis repellat optio amet ratione
+			nesciunt fuga.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id aspernatur vel debitis earum
+			suscipit perspiciatis fugit officia ut reiciendis officiis tempore maiores nihil. Eum explicabo dolores quod libero
+			delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi alias hic quod quisquam. Libero delectus, quas
+			minima distinctio facilis obcaecati rerum quidem aliquam quis repellat optio amet ratione nesciunt fuga.
+		</div>
 	)
 }
 
